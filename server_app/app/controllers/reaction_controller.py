7bb07252e2a5c4855d670fba4145eb6c94c39f7e
pavelihno <crazzyyellow02@gmail.com>
2024-05-11 19:47:22 +0300

    removed redundant async dependencies (redis and celery)

--- server_app/app/controllers/reaction_controller.py
+++ server_app/app/controllers/reaction_controller.py
@@ -3,7 +3,7 @@ from flask import request, jsonify
 from app.models.reaction import Reaction
 from app.models.profile import Profile
 from app.middlewares.auth_middleware import login_required
-from app.celery_app import identify_potential_allergens_task
+from app.controllers.allergen_controller import identify_potential_allergens
 
 @login_required
 def create_reaction(current_user, profile_id):
@@ -12,7 +12,7 @@ def create_reaction(current_user, profile_id):
     if profile and profile.user_id == current_user.id:
         new_reaction = Reaction.create(**body)
         if new_reaction:
-            identify_potential_allergens_task.delay(profile.id, new_reaction.id)
+            identify_potential_allergens(profile.id, new_reaction.id)
             return jsonify(new_reaction.to_dict()), 201
     return jsonify({'message': 'Failed to create reaction'}), 400
 
@@ -40,7 +40,7 @@ def update_reaction(current_user, profile_id, reaction_id):
         body = request.json
         updated_reaction = Reaction.update(reaction_id, **body)
         if updated_reaction:
-            identify_potential_allergens_task.delay(profile.id, updated_reaction.id)
+            identify_potential_allergens(profile.id, updated_reaction.id)
             return jsonify(updated_reaction.to_dict())
     return jsonify({'message': 'Reaction not found'}), 404
 
@@ -50,6 +50,6 @@ def delete_reaction(current_user, profile_id, reaction_id):
     if profile and profile.user_id == current_user.id:
         deleted_reaction = Reaction.delete(reaction_id)
         if deleted_reaction:
-            identify_potential_allergens_task.delay(profile.id, deleted_reaction.id)
+            identify_potential_allergens(profile.id, deleted_reaction.id)
             return jsonify(deleted_reaction.to_dict())
     return jsonify({'message': 'Reaction not found'}), 404
